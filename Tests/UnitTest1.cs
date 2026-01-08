d6a0de565bc75e494457370dc6130c58b3ee8503
Student <foobar@sr.htlweiz.at>
2026-01-08 15:20:22 +0100

    more tests done

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -4,6 +4,17 @@ namespace Tests;
 public class Test1
 {
     [Fact]
+    public void Test_single_element()
+    {
+        AverageCalculator calc = new AverageCalculator();
+        calc.add(54);
+        double[] expected = {54};
+        for (int i = 0; i < expected.Length; i++)
+        {
+            Assert.Equal(expected[i], calc.getElements()[i], precision: 4);
+        }
+    }
+    [Fact]
     public void Test_normal_add_classic()
     {
         AverageCalculator calc = new AverageCalculator();
@@ -17,11 +28,9 @@ public class Test1
     }
 
     [Fact]
-    public void test_min_add_classic(){
+    public void test_empty_array(){
         AverageCalculator calc = new AverageCalculator();
-        calc.add();
-        calc.add(2);
-        double[] expected = {34, 2};
+           double[] expected = {};
         for (int i = 0; i < expected.Length; i++)
         {
             Assert.Equal(expected[i], calc.getElements()[i], precision: 4);
